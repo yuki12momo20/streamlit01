877aeb4e65c979f05e4e6248cddd654fb5b13008
Yuki <TTC2550SA0003@edu.tech.ac.jp>
2025-11-27 11:26:40 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,24 @@
-import streamlit as st
+# -*- coding: utf-8 -*-
+# -*- coding: utf-8 -*-
 
-st.title("テストページ")
-st.write("これはテストページです。")
+import streamlit as st
+import numpy as np
+import pandas as pd
+
+st.title("Web App")
+st.write("**これがstreamlitアプリ**")
+
+answer = st.button('Say hello')
+
+if answer == True:
+     st.write('Why hello there')
+else:
+     st.write('Goodbye')
+    
+data = {
+    'lat': np.random.randn(100) / 100 + 35.68,
+    'lon': np.random.randn(100) / 100 + 139.75,
+}
+map_data = pd.DataFrame(data)
+# 地図に散布図を描く
+st.map(map_data)
